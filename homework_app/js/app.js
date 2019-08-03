c5f522f4f8a5bee2ca5af7c9edaa8ce29b71733c
mikefallon <mikefallon81@gmail.com>
2019-08-03 14:29:26 +0100

    initial commit

---
+++ homework_app/js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const playerProfile = document.querySelector('#playerProfileList');
+
+  const button = document.querySelector('#new-item-form');
+  button.addEventListener('submit', (e) => {
+    console.dir(e.target);
+    e.preventDefault();
+
+
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('list-item');
+
+  const nameEntry = document.createElement('h2');
+  nameEntry.textContent = e.target.name.value;
+  newDiv.appendChild(nameEntry);
+
+  const positionEntry = document.createElement('h3');
+  positionEntry.textContent = e.target.position.value;
+  newDiv.appendChild(positionEntry);
+
+  const ageEntry = document.createElement('h3');
+  ageEntry.textContent = e.target.age.value;
+  newDiv.appendChild(ageEntry);
+
+  playerProfile.appendChild(newDiv);
+
+});
+
+});
